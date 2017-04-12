f5b96164ad1160ca5c88aa3e1d87b47d81fe648c
zhangningningcn <442881592@qq.com>
2017-04-12 20:10:16 +0800

    显示误差+-值

--- Resistors.py
+++ Resistors.py
@@ -1,8 +1,16 @@
 # -*- encoding:utf-8 -*-
 import re
 import math
-
+#E6  20%
+#NominalResistors_6 = [1.0,1.5,2.2,3.3,4.7,6.8]
+#E12 10%
+#NominalResistors_12 = [1.0,1.2,1.5,1.8,2.2,2.7,3.3,3.9,4.7,5.6,6.8,8.2]
+#E96 1%
+#NominalResistors_96 = [round(math.pow(10,i/96), 2) for i in range(96)] 
+#E24 5%
 NominalResistors = [1.0,1.1,1.2,1.3,1.5,1.6,1.8,2.0,2.2,2.4,2.7,3.0,3.3,3.6,3.9,4.3,4.7,5.1,5.6,6.2,6.8,7.5,8.2,9.1]
+DataNum = len(NominalResistors)
+
 AllNominalResistors = None
 AllNominalResistors_1 = None  # 1/AllNominalResistors
 ResistorsParalleling = []
@@ -56,7 +64,7 @@ def MinNumAndIndexSeries(i1,i2,v):
 def FindNearestValueSeries(t_value):
     global AllNominalResistors
     t = t_value/AllNominalResistors[0]
-    index = int(math.log10(t)*24)
+    index = int(math.log10(t)*DataNum)
     min = MinNumAndIndexSeries(index,index+1,t_value)
     return min
 
@@ -76,6 +84,11 @@ def Series(t_value):
             v1 = AllNominalResistors[min[0]]
             AddMinToList(ResistorsSeries,[v1,0,min[1]])
 
+    for i in ResistorsSeries:
+        if i[1]:
+            i[2] = i[0] + i[1] - t_value
+        else:
+            i[2] = i[0] - t_value
 
 def MinNumAndIndexParalleling(i1,i2,v):
     v1 = abs(AllNominalResistors_1[i1] - v)
@@ -88,7 +101,7 @@ def MinNumAndIndexParalleling(i1,i2,v):
 def FindNearestValueParalleling(t_value):
     global AllNominalResistors_1
     t = 1/(t_value*AllNominalResistors[0])
-    index = int(len(AllNominalResistors_1)-math.log10(t)*24)
+    index = int(len(AllNominalResistors_1)-math.log10(t)*DataNum)
     min = MinNumAndIndexParalleling(index,index-1,t_value)
     return min
 
@@ -113,18 +126,18 @@ def Paralleling(t_value):
 
     for i in ResistorsParalleling:
         if i[1]:
-            i[2] =abs(t_value - i[0]*i[1]/(i[0] + i[1]))
+            i[2] = i[0]*i[1]/(i[0] + i[1]) - t_value
         else:
-            i[2] =abs(t_value - i[0])
+            i[2] = i[0] - t_value
 
 
 def ValToString(val):
     if val is None:
         return '   inf'
     if val < 1000:
-        return "{: =6.1f}".format(val)
+        return "{:6.1f}".format(val)
     else:
-        return "{: =5.1f}k".format(val/1000)
+        return "{:5.1f}k".format(val/1000)
 
 if __name__ == "__main__":
 
@@ -147,9 +160,11 @@ if __name__ == "__main__":
             print("请输入有效数值")
     Series(R_Value)
     print("串联:")
+    print("    R1,    R2,  误差")
     for i in ResistorsSeries:
         print("{},{},{}".format(ValToString(i[0]),ValToString(i[1]),ValToString(i[2])))
     Paralleling(R_Value)
     print("并联:")
+    print("    R1,    R2,  误差")
     for i in ResistorsParalleling:
         print("{},{},{}".format(ValToString(i[0]),ValToString(i[1]),ValToString(i[2])))
