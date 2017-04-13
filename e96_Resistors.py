dcf87ddc87d88b84cf17a580797986e372d25f76
zhangningningcn <442881592@qq.com>
2017-04-13 21:00:18 +0800

    添加E96标准编码电阻查询

---
+++ e96_Resistors.py
@@ -0,0 +1,81 @@
+# -*- encoding:utf-8 -*-
+import math
+import re
+
+CharacterExp={"Z":-3,"Y":-2,"X":-1,"A":0,"B":1,"C":2,"D":3,"E":4,"F":5}
+
+
+def GetInputString():
+    code_num = 0
+    code_exp = None
+    s_type = 0
+    while True:
+        codestr = input("请输入编码或者电阻值:")
+        codestr = codestr.upper()
+        if codestr == 'Q':
+            return (3,None,None)
+        if not re.match(r'^\d\d[A-FX-Z]$',codestr):
+            if not re.match(r'^\d+.?\d*[KM]?$',codestr):
+                print("输入代码有误，请重新输入")
+                continue
+            s_type = 1
+        if s_type == 0:
+            code_num = int(codestr[:2]) - 1
+            if code_num >= 96:
+                print("输入代码有误，请重新输入")
+                continue
+            code_exp = CharacterExp.get(codestr[2],None)
+            if code_exp == None:
+                print("输入代码有误，请重新输入")
+                continue
+        else:
+            #codestr = codestr.lower()
+            if 'K' in codestr:
+                code_exp = 3
+                codestr = codestr[:-1]
+            elif 'M' in codestr:
+                code_exp = 6
+                codestr = codestr[:-1]
+            else:
+                code_exp = 0
+            code_num = float(codestr)
+        break
+    return(s_type,code_num,code_exp)
+def DecodeResistorE96(code_num,code_exp):
+
+    value = round(math.pow(10,code_num/96),2) * math.pow(10,code_exp+2)
+    if value >= 1000000:
+        return("{:g}M".format(value/1000000))
+    elif value >= 1000:
+        return("{:g}k".format(value/1000))
+    else:
+        return("{:g}".format(value))
+
+def EncodeResistorE96(code_num,code_exp):
+    R_Value = code_num * math.pow(10,code_exp)
+    log_num = math.log10(R_Value)
+    r_exp = math.floor(log_num)
+    value = int((log_num - r_exp) * 96 + 0.5)
+    r_exp = int(r_exp) - 2
+    str_exp = None
+    for k,v in CharacterExp.items():
+        if v == r_exp:
+            str_exp = k
+            break
+    if str_exp == None:
+        return "超范围"
+    str_cpde = "{:0=2}{}".format(value+1,str_exp)
+    return str_cpde + '(' + DecodeResistorE96(float(value),float(r_exp)) + ')'
+
+
+
+if __name__ == "__main__":
+    while True:
+        s_type,code_num,code_exp = GetInputString();
+        if s_type == 0:
+            print(DecodeResistorE96(code_num,code_exp))
+        elif s_type == 1:
+            print(EncodeResistorE96(code_num,code_exp))
+        else:
+            break
+
